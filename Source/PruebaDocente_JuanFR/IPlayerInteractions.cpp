ea8af2cce2cbd926ae8ffcf441a79d69b62015d8
Thingy25 <juanfr25@yahoo.es>
2025-12-12 00:36:29 -0500

    Player interactions interface
    Created interface to handle player interactions such as calling animations, effects, etc.

---
+++ Source/PruebaDocente_JuanFR/IPlayerInteractions.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IPlayerInteractions.h"
+
+// Add default functionality here for any IIPlayerInteractions functions that are not pure virtual.
---
+++ Source/PruebaDocente_JuanFR/IPlayerInteractions.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IPlayerInteractions.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UIPlayerInteractions : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PRUEBADOCENTE_JUANFR_API IIPlayerInteractions
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void PlayInteractAnimation();
+};
